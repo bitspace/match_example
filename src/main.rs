36758fdfd65f8e22256bc2029edbe32b7c26d510
Chris Woods <chris.woods@gmail.com>
2022-01-10 21:15:39 -0500

    pattern bind to a value

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -13,7 +19,10 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
